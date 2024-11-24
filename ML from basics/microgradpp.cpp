b8168a2c9e6833e0fe35cfca735a9054d2cf94c8
Gautam <gautamsharma2813@gmail.com>
2024-11-24 09:01:41 -0500

    added Neuron class - Part 4

--- ML from basics/microgradpp.cpp
+++ ML from basics/microgradpp.cpp
@@ -105,6 +105,18 @@ public:
         return out;
     }
 
+
+    static ValuePtr sigmoid(const ValuePtr& input) {
+        float x = input->data;
+        float t = std::exp(x) / (1 + std::exp(x));
+        auto out = Value::create(t, "Sigmoid");
+        out->prev = {input};
+        out->backward = [input, out]() {
+            input->grad += t * (1 - t) * out->grad;
+        });
+        return out;
+    }
+
     
     void buildTopo(std::shared_ptr<Value> v, std::unordered_set<std::shared_ptr<Value>, Hash>& visited, std::vector<std::shared_ptr<Value>>& topo){
          if(visited.find(v) == visited.end()){
@@ -142,6 +154,140 @@ public:
 size_t Hash::operator()(const ValuePtr value) const {
     return std::hash<std::string>()(value.get()->op) ^ std::hash<float>()(value.get()->data);
 }
+
+
+// Part 4
+
+enum ActivationType{
+    RELU,
+    SIGMOID
+};
+
+class Activation{
+    static std::shared_ptr<Value> Relu(const std::shared_ptr<Value>& val){
+        return Value::relu(val);
+    }
+    static std::shared_ptr<Value> Sigmoid(const std::shared_ptr<Value>& val){
+        return Value::sigmoid(val);
+    }
+
+public:
+    static inline std::unordered_map<ActivationType, std::function<std::shared_ptr<Value>(std::shared_ptr<Value>&)>> mActivationFcn = {
+            {ActivationType::RELU, Relu},
+            {ActivationType::SIGMOID, Sigmoid}
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Function to generate a random float between -1 and 1
+float getRandomFloat() {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    static std::uniform_real_distribution<> dis(-1, 1);
+    return dis(gen);
+}
+
+class Neuron {
+private:
+    std::vector<ValuePtr> weights;
+    ValuePtr bias = Value::create(0.0);
+    const ActivationType activation_t;
+
+public:
+    Neuron(size_t nin, const ActivationType& activation_t) : activation_t(activation_t) {
+        for (size_t idx = 0; idx < nin; ++idx) {
+            weights.emplace_back(Value::create(getRandomFloat()));
+        }
+    }
+
+//    // For testing
+//    Neuron(size_t nin, float val, const ActivationType& activation_t = ActivationType::SIGMOID)
+//            : activation_t(activation_t) {
+//        for (size_t idx = 0; idx < nin; ++idx) {
+//            weights.emplace_back(Value::create(getRandomFloat()));
+//        }
+//    }
+
+    void zeroGrad() {
+        for (auto& weight : weights) {
+            weight->grad = 0;
+        }
+        bias->grad = 0;
+    }
+
+    // Dot product of a Neuron's weights with the input
+    ValuePtr operator()(const std::vector<ValuePtr>& x) {
+        if (x.size() != weights.size()) {
+            throw std::invalid_argument("Vectors must be of the same length");
+        }
+
+        ValuePtr sum = Value::create(0.0);
+
+        for (size_t idx = 0; idx < weights.size(); ++idx) {
+
+            ValuePtr intermediateVal = Value::multiply(x[idx], weights[idx]);
+            sum = Value::add(sum, intermediateVal);
+        }
+
+        // Add bias
+        //sum->add_inplace(bias);
+        sum = Value::add(sum, bias);
+
+        // Apply activation function
+        const auto& activationFcn = Activation::mActivationFcn.at(activation_t);
+        return activationFcn(sum);
+    }
+
+    std::vector<ValuePtr> parameters() const {
+        std::vector<ValuePtr> out;
+        out.reserve(weights.size() + 1);
+
+        out.insert(out.end(), weights.begin(), weights.end());
+        out.push_back(bias);
+
+        return out;
+    }
+
+    void printParameters() const {
+        printf("Number of Parameters: %zu\n", weights.size() + 1);
+        for (const auto& param : weights) {
+            printf("%f, %f\n", param->data, param->grad);
+        }
+        printf("%f, %f\n", bias->data, bias->grad);
+        printf("\n");
+    }
+
+    size_t getParametersSize() const {
+        return weights.size() + 1;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
